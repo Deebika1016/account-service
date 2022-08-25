4988732924fd2c587631a69435c6c7cca36d5a39
Ramakanth A <ramakantha@maveric-systems.com>
2022-08-25 22:10:17 +0530

    Created Happy paths for update account feature

--- src/main/java/com/maveric/accountservice/controller/AccountController.java
+++ src/main/java/com/maveric/accountservice/controller/AccountController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1")
 public class AccountController {
@@ -16,27 +14,8 @@ public class AccountController {
     @Autowired
     AccountService accountService;
 
-    @GetMapping("/customers/{customerId}/accounts")
-    public ResponseEntity<List<AccountDto>> getAccountdetails(@PathVariable String customerId, @RequestParam(defaultValue = "0") Integer page,
-                                                              @RequestParam(defaultValue = "10") Integer pageSize) {
-        List<AccountDto> accountdtoresult = accountService.getAccountDetails();
-        return new ResponseEntity<List<AccountDto>>(accountdtoresult, HttpStatus.OK);
-    }
-   @PostMapping("/customers/{customerId}/accounts")
-   public ResponseEntity<AccountDto> createAccount(@PathVariable String customerId, @RequestBody AccountDto accountDto) {
-       AccountDto accountdtoresult = accountService.createAccount(accountDto);
-       return new ResponseEntity<AccountDto>(accountdtoresult, HttpStatus.CREATED);
-   }
-   @GetMapping("/customers/{customerId}/accounts/{accountId}")
-   public ResponseEntity<AccountDto> getaccountbtId(@PathVariable String customerId ,@PathVariable String accountId){
-        AccountDto result = accountService.getaccountbyId(accountId);
-        return new ResponseEntity<AccountDto>(result,HttpStatus.OK);
-   }
-   @DeleteMapping("/customers/{customerId}/accounts/{accountId}")
-    public ResponseEntity<String> deleteAccount(@PathVariable String customerId, @PathVariable String accountId){
-        String result = accountService.deleteAccount(accountId);
-       return new ResponseEntity<String>(result, HttpStatus.OK);
-   }
+
+
     @PutMapping("customer/{customerId}/accounts/{accountId}")
     public ResponseEntity<AccountDto> updateAccount(@PathVariable String customerId,@PathVariable String accountId,@RequestBody AccountDto accountDto) {
         AccountDto accountDtoResponse = accountService.updateaccount(accountId,accountDto);
--- src/main/java/com/maveric/accountservice/service/AccountServiceImp.java
+++ src/main/java/com/maveric/accountservice/service/AccountServiceImp.java
@@ -8,38 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.AccountNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static com.maveric.accountservice.Methods.MapModelandDto.*;
+import static com.maveric.accountservice.Methods.MapModelandDto.getCurrentDateTime;
+import static com.maveric.accountservice.Methods.MapModelandDto.toDto;
 
 @Service
 public class AccountServiceImp implements AccountService {
     @Autowired
     private AccountRepository repository;
 
-    @Override
-    public List<AccountDto> getAccountDetails() {
-        List<Account> list= repository.findAll();
-        List<AccountDto> listdto =new ArrayList<AccountDto>(list.size());
-        for(Account account:list)
-        {
-            listdto.add(toDto(account));
-        }
-        return  listdto;
-    }
 
-    @Override
-    public AccountDto createAccount(AccountDto accountDto) {
-        accountDto.setCreatedAt(getCurrentDateTime());
-        accountDto.setUpdatedAt(getCurrentDateTime());
-        Account account = null;
-        account.setCustomerId(accountDto.getCustomerId());
-        account.setType(accountDto.getType());
-//        Account account = toEntity(accountDto);
-//        Account accountResult = repository.save(account);
-        return  toDto(account);
-    }
+
+
+
     @SneakyThrows
     @Override
     public AccountDto updateaccount(String accountId,AccountDto accountDto)
@@ -55,17 +36,6 @@ public class AccountServiceImp implements AccountService {
         return toDto(accountUpdated);
     }
 
-    @SneakyThrows
-    @Override
-    public AccountDto getaccountbyId(String accountid) {
-        Account accountResult=repository.findById(accountid).orElseThrow(() -> new AccountNotFoundException("Account not found"));
-        return toDto(accountResult);
 
-    }
 
-    @Override
-    public String deleteAccount(String accountId) {
-        repository.deleteById(accountId);
-        return "account deleted successfully.";
-    }
 }
