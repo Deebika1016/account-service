58e73ccd9ae62b722efc1c0113e39e2f29990b15
Ramakanth A <ramakantha@maveric-systems.com>
2022-09-15 13:50:28 +0530

    Added happy paths and Unit test cases

---
+++ src/main/java/com/maveric/accountservice/dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.maveric.accountservice.dto;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class ErrorDto {
+    String code;
+    String message;
+}
